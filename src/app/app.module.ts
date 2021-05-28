c569ea50888aba5dbce206f52560488c98eb79e5
Marco Silva <marcosilva.tec@gmail.com>
2021-05-28 12:31:03 -0300

    Create Array of Object, problem solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
-import { SalesPersonClassComponent } from './sales-person-class/sales-person-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SalesPersonListComponent,
-    SalesPersonClassComponent
   ],
   imports: [
     BrowserModule
