f7ad8a838c833e0a931bca30a12c604301d744f7
Marco Silva <marcosilva.tec@gmail.com>
2021-05-28 12:16:50 -0300

    Creating the first class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
+import { SalesPersonClassComponent } from './sales-person-class/sales-person-class.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalesPersonListComponent,
+    SalesPersonClassComponent
   ],
   imports: [
     BrowserModule
