d9af4845387444b79e47697425dfe32385691e9a
Marco Silva <marcosilva.tec@gmail.com>
2021-05-28 12:26:43 -0300

    Create Array of Object

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Marco", "Silva", "marcosilva.tec@hotmail.com", 90000),
+    new SalesPerson("Ale", "Silva", "ale@hotmail.com", 90000),
+    new SalesPerson("Daya", "Silva", "dayana@gmail.com", 9000),
+    new SalesPerson("Aline", "Silva", "aline@hotmail.com", 90000)
+  ];
 
   constructor() { }
 
